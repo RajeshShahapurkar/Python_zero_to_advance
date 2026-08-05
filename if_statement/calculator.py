42ea71e9416a05973ea650720ba7681313c1123b
RajeshShahapurkar <rajeshshahapurkar25@gmail.com>
2026-08-05 23:27:14 +0530

    if statement

---
+++ if_statement/calculator.py
@@ -0,0 +1,22 @@
+num1=int(input("Emter the first number: "))
+num2=int(input("Emter the second number: "))
+
+operator=input("Enter the operation to perform(+,-,*,/,%): ")
+
+if operator=="+":
+    print(num1+num2)
+elif operator=="-":
+    print(num1-num2)
+elif operator=="*":
+    print(num1*num2)
+elif operator=="/":
+    if num2==0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        print(num1/num2)
+elif operator=="%":
+    if num2==0:
+        print("Error: Division by zero is not allowed.")
+    else:
+        print(num1%num2)
+        
